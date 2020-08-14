e5dc1bbafe2ccb8dde5662f4392f56c2da733488
hai14894 <hohoanghai14894@gmail.com>
2020-08-14 22:40:51 +1000

    successfully dispatch the actions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
+import {increment, decrement} from './actions'
 
 
 function App() {
   const counter = useSelector(state =>state.counterReducer)
+  const isLogged = useSelector(state =>state.isLogged)
+  const dispatch = useDispatch()
   return (
     <div className="App">
       <h1>COUNTER: {counter} </h1>
+      <button onClick={() => {dispatch(increment())}}>+</button>
+      <button onClick={() => {dispatch(decrement())}}>-</button>
+      {isLogged &&
+      <h3>This text only show if logged in</h3>}
     </div>
   );
 }
