64dc57e69c740ab1ed86c072e845aa867e2c2e70
hai14894 <hohoanghai14894@gmail.com>
2020-08-14 22:43:51 +1000

    now use payload instead of fixed number in reducer

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <div className="App">
       <h1>COUNTER: {counter} </h1>
-      <button onClick={() => {dispatch(increment())}}>+</button>
-      <button onClick={() => {dispatch(decrement())}}>-</button>
+      <button onClick={() => {dispatch(increment(3))}}>+</button>
+      <button onClick={() => {dispatch(decrement(19))}}>-</button>
       {isLogged &&
       <h3>This text only show if logged in</h3>}
     </div>
